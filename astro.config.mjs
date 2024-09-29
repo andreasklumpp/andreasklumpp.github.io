5e93909c1431ab5620037d5993611f02a11f23bf
andreasklumpp <andreas@klumpp-besigheim.de>
2024-09-29 17:27:50 +0200

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,31 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+import rehypeSlug from 'rehype-slug';
+import rehypePrettyCode from 'rehype-pretty-code';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx({
+        syntaxHighlight: false,
+        rehypePlugins: [
+          /**
+           * Adds ids to headings
+           */
+          rehypeSlug,
+          [
+            /**
+             * Enhances code blocks with syntax highlighting, line numbers,
+             * titles, and allows highlighting specific lines and words
+             */
+   
+            rehypePrettyCode,
+            {
+              theme: 'github-dark',
+            },
+          ],
+        ],
+      }),, sitemap(), tailwind()],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,18 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    // using zod to define type-safe frontmatter of our mdx files
+    // astro will generate types definitions for our project so we can use them in templates
+    // also it will check every newly created frontmatter in the content/blog directory
+    z.object({
+      title: z.string(),
+      tags: z.array(z.string()),
+      date: z.coerce.date(),
+      excerpt: z.string(),
+    }),
 });
 
 export const collections = { blog };
