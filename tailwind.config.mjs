cbe71e41d08193722762966795a43a60ea583b1f
andreasklumpp <andreas@klumpp-besigheim.de>
2025-07-19 17:23:36 +0200

    Simplify page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				"roboto": ["Roboto", "sans-serif"],
-				"bebasNeue": ["Bebas Neue", "sans-serif"]
-			}
-		},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        bebasNeue: ["Bebas Neue", "sans-serif"],
+        archivoBlack: ["Archivo Black", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
